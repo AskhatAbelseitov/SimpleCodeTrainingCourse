d0927e17a89cf215a578b8ccde57a6a88d19f148
Askhat Abelseitov <askhat_abelseitov@epam.com>
2024-02-12 10:01:06 +0600

    Add project files.

---
+++ Task 1 - Average value/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue, averageValue;
+
+            Console.WriteLine("Enter your first number");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your first number is: {firstValue}");
+
+            Console.WriteLine("Enter your second number");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your second number is: {secondValue}");
+
+            averageValue = (firstValue + secondValue) / 2;
+
+            Console.WriteLine($"Average value is: {averageValue}");
+        }
+    }
+}
+
---
+++ Task 2 - Adding & multiplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue, thirdValue, addValue, multValue;
+
+            Console.WriteLine("Enter your first number");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your first number is: {firstValue}");
+
+            Console.WriteLine("Enter your second number");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your second number is: {firstValue}");
+
+            Console.WriteLine("Enter your third number");
+
+            thirdValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your third number is: {secondValue}");
+
+            addValue = firstValue + secondValue + thirdValue;
+
+            multValue = firstValue * secondValue * thirdValue;
+
+            Console.WriteLine($"Adding result is: {addValue} and multiplication result is: {multValue}");
+        }
+    }
+}
+
