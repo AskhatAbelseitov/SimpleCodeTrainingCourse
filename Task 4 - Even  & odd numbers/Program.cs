1a74712d98687a42ac9c4440575fd6738965ab3d
Askhat Abelseitov <askhat_abelseitov@epam.com>
2024-02-12 14:32:39 +0600

    Task 4 added

---
+++ Task 4 - Even  & odd numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you number");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a%2==0)
+            {
+                Console.WriteLine("Entered number is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is odd");
+            }
+        }
+    }
+}
---
+++ Working Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fanSpeed = 3000;
+
+            bool isHighTemperature = false;
+
+            bool hasNoColling = fanSpeed <= 10;
+
+            if (hasNoColling || isHighTemperature)
+            {
+                Console.WriteLine("Processor is broken!");
+            }
+        }
+    }
+}
