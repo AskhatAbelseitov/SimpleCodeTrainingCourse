41f03a0fcd5b23bf5ff6bdf760cc38a55fa89a99
Askhat Abelseitov <askhat_abelseitov@epam.com>
2024-02-12 10:19:30 +0600

    Task 3 added

---
+++ Task 3 - Converter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollarRate = 442.34;
+            double euroRate = 482.56;
+            double nationalCurrency;
+            
+            Console.WriteLine("Enter your sum in the national currency");
+
+            nationalCurrency = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your sum is {nationalCurrency} tenge");
+
+            double dollarCurrency  = nationalCurrency / dollarRate;
+
+            double euroCurrency = nationalCurrency / euroRate;
+
+            Console.WriteLine($"Your exchange course is: {dollarCurrency} dollars or {euroCurrency} euro");
+        }
+    }
+}
+
